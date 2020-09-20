34fc79f3097b12011a82b0612e4f6c17765ee0e4
Isaiah Davis <idavis@igainc.com>
2020-09-19 19:32:06 -0500

    home page styling

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -4,10 +4,10 @@ import style from './style';
 
 const Header = () => (
 	<header class={style.header}>
-		<Link href="/"><h1>Diversey Housee</h1></Link>
+		<Link href="/"><h1>Diversey House</h1></Link>
 		<nav>
-			<Link activeClassName={style.active} href="/blogs">Blogs</Link>
-			<Link activeClassName={style.active} href="/contact">Contact me</Link>
+			{/* <Link activeClassName={style.active} href="/blogs">Blogs</Link> */}
+			{/* <Link activeClassName={style.active} href="/contact">Contact me</Link> */}
 		</nav>
 	</header>
 );
